d46cb1dcd83d9633c012d3956e2b359b67b0cb5b
EddyEscalanteUstariz <78371198+EddyEscalanteUstariz@users.noreply.github.com>
2023-08-21 20:11:29 -0400

    frontend v5

--- frontend/frontend-upc/src/app/tab1/tab1.page.ts
+++ frontend/frontend-upc/src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Usuarios } from '../entidades/usuarios';
 import { UsuariosService } from '../servicios-backend/usuarios/usuarios.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -16,7 +17,7 @@ export class Tab1Page {
   public listaUsuarios: Usuarios[] = []
 
   constructor(private usuariosService: UsuariosService) {
-
+/*
     let usuario: Usuarios = new Usuarios();
     usuario.nombreCompleto = "Eddy Escalante"
     usuario.userName = "eescalante"
@@ -24,14 +25,59 @@ export class Tab1Page {
 
     this.listaUsuarios.push(usuario)
     this.listaUsuarios.push(usuario)
-
-    'usuariosService.GetUsuarios()
-
+*/
+    this.getUsuariosFromBackend();
   }
 
+  private getUsuariosFromBackend(){
+    this.usuariosService.GetUsuarios().subscribe({
+        next: (response: HttpResponse<any>) => {
+            this.listaUsuarios = response.body;
+            console.log(this.listaUsuarios)
+        },
+        error: (error: any) => {
+            console.log(error);
+        },
+        complete: () => {
+            //console.log('complete - this.getUsuarios()');
+        },
+    });
+  }
 
   public addUsuario(){
+   //console.log(this.nombreCompleto) 
+   //console.log(this.userName) 
+   //console.log(this.password) 
+   this.AddUsuarioFromBackend(this.nombreCompleto, this.userName, this.password)
+  }
 
+  private AddUsuarioFromBackend(nombreCompleto: string, userName: string, password: string){
+
+    var usuarioEntidad = new Usuarios();
+    usuarioEntidad.nombreCompleto = nombreCompleto;
+    usuarioEntidad.userName = userName;
+    usuarioEntidad.password = password;
+
+    this.usuariosService.AddUsuario(usuarioEntidad).subscribe({
+      next: (response: HttpResponse<any>) => {
+          console.log(response.body)//1
+          if(response.body == 1){
+              alert("Se agrego el USUARIO con exito :)");
+              this.getUsuariosFromBackend();//Se actualize el listado
+              this.nombreCompleto = "";
+              this.userName = "";
+              this.password = "";
+          }else{
+              alert("Al agregar al USUARIO fallo exito :(");
+          }
+      },
+      error: (error: any) => {
+          console.log(error);
+      },
+      complete: () => {
+          console.log('complete - this.AddUsuario()');
+      },
+  });
   }
 
 }
