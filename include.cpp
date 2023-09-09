56c78d9daa047040acd03d35b15d901f08ca1b80
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-09-09 11:43:34 -0400

    ucb

---
+++ include.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int func_fibonacci (int n){
+         
+};
+
+int main () {
+
+cout << "intrduce dos numeros: ";
+cin >> n
+
+func_fibonacci(int n){
+  if (n <= 1) {
+    return n;
+    }
+  else {
+
+
+    return func_fibonacci; 
+  }
+
+}  
+
+return 0;
+}
